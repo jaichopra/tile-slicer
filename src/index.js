a972d33b9263b0c7257b5d0267f4447f74bc9989
Jai Chopra <jaichopra@gmail.com>
2017-04-11 16:56:56 +1000

    add some quick validation

--- src/index.js
+++ src/index.js
@@ -2,10 +2,18 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 import { TileSlicer } from './TileSlicer';
 import { Resizer } from './Resizer';
 const path = require('path');
+const fs = require('fs');
 require('events').EventEmitter.prototype._maxListeners = 1000;
 
 const run = async () => {
-  const levels = await Resizer.generateLevels(path.join(__dirname, '../data/Cat.jpg'));
+  if (process.argv.length < 3) {
+    throw new Error('Please provide an image file path relative to the local dir');
+  }
+  const file = path.join(__dirname, '../', process.argv[2]);
+  if (!fs.existsSync(file)) {
+    throw new Error(`Please ensure the file exists at the path ${file}`);
+  }
+  const levels = await Resizer.generateLevels(file);
   const slicer = new TileSlicer();
   for (const level of levels) {
     console.log(`Attempting to slice tiles for level ${level.value}`);
@@ -16,5 +24,5 @@ const run = async () => {
 run()
 .then()
 .catch(err => {
-  console.log(err.stack);
+  console.log(err);
 });
